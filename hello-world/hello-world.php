6e9670ed6e11a85f53764ffa1e37d7f81ab62ba5
Thibault Seynaeve <thibault.seynaeve@iodigital.com>
2024-07-31 11:00:35 +0200

    simple demo plugin that echo's "hello world"

---
+++ hello-world/hello-world.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: Hello World Plugin
+Description: A simple plugin that echoes "Hello World".
+Version: 1.0
+Author: Your Name
+*/
+
+// Function to echo "Hello World"
+function hello_world() {
+    echo '<p>Hello World</p>';
+}
+
+// Hook the function to the WordPress 'wp_footer' action
+add_action('wp_footer', 'hello_world');
